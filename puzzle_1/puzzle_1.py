a7e0e933027f11446c82d18455daf20d43f36c2a
Jaysen <jaysenstolarz@gmail.com>
2023-12-02 17:05:56 -0500

    gtrwegtre

---
+++ puzzle_1/puzzle_1.py
@@ -0,0 +1,20 @@
+import re
+
+
+def count(filename):
+    with open(filename) as file:
+        count = 0
+        for line in file:
+            nums = re.findall("[0-9]", line)
+            coord = int(str(nums[0]) + str(nums[-1]))
+            count += coord
+    return count
+
+
+
+
+
+def main():
+    print(count('day1input.txt'))
+
+main()
