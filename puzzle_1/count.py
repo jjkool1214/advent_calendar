8cf3aaf8aaf20098dc86207a125e15cb619fccad
Jaysen <jaysenstolarz@gmail.com>
2023-12-02 17:33:51 -0500

    done for now

--- puzzle_1/count.py
+++ puzzle_1/count.py
@@ -4,14 +4,27 @@ import re
 def count(filename):
     with open(filename) as file:
         count = 0
+        lines = 0
         for line in file:
             nums = re.findall("[0-9]", line)
             coord = int(str(nums[0]) + str(nums[-1]))
             count += coord
+            lines += 1
+    return count, lines
+
+def count_2(lst):
+    count = 0
+    for line in lst:
+        nums = re.findall("[0-9]", str(line))
+        coord = int(str(nums[0]) + str(nums[-1]))
+        count += coord
     return count
 
 
 def main():
-    print(count("input.txt"))
+    lst = [["1abc2"], ['pqr3stu8vwx'], ['a1b2c3d4e5f'], ['treb7uchet']]
+    print(count_2(lst))
 
-main()
+    print(count("C:\\advent_calendar_of_code\\advent_calendar\puzzle_1\\input.txt"))
+
+main()gtrnmeigotre
